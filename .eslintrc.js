5314792a528d9a96d7ce344798457728f2c682e2
Juuso Lehtinen <jupelehtinen@gmail.com>
2018-11-25 17:58:40 +0200

    Updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     "extends": "airbnb",
     "parser": "babel-eslint",
+    "env": {
+        "browser": true
+    },
     "rules": {
         "comma-dangle": ["error", "never"],
         "jsx-a11y/anchor-is-valid": [ "error", {
@@ -8,6 +11,7 @@ module.exports = {
             "specialLink": [ "to", "hrefLeft", "hrefRight" ],
             "aspects": [ "noHref", "invalidHref", "preferButton" ]
           }],
-        "react/no-typos": "off"
+        "react/no-typos": "off",
+        "react/jsx-filename-extension": "off"
     }
 };
